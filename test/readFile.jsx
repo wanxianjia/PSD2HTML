16e149d98a8336030f3774f80bc21a53308fe583
majorye <major.yezhouquan@gmail.com>
2012-05-11 18:19:11 +0800

    correct the js file

--- test/readFile.jsx
+++ test/readFile.jsx
@@ -1,13 +1,14 @@
-#target photoshop
+﻿#target photoshop
 
 /**
 * @author: zhouquan.yezq
 * @date : 5/11 2012
 * @description: the demo for read html fragement
-/
+*/
 
 function main(){
-	var f =  File(scriptsFile.parent + "/comps/nav.html");
+    var scriptsFile = new File($.fileName);
+	var f =  File(scriptsFile.parent + "/comps/comp_thirdpf_header.html");
      f .encoding = 'UTF-8';
      f .open('r');
      while(!f.eof){
