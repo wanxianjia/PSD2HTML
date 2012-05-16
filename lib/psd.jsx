00ddf25f5b47193bd144200c318ec738b37f4cac
wanxianjia <okwxj@qq.com>
2012-05-16 10:30:29 +0800

    fixed layer bounds

--- lib/psd.jsx
+++ lib/psd.jsx
@@ -3,9 +3,6 @@
 //setting for app preferences
 app.preferences.rulerUnits = Units.PIXELS;
 app.preferences.typeUnits = TypeUnits.PIXELS;
- 
- //current script file 
-var scriptsFile = new File($.fileName);
      
      
 /*****************************************************************************
@@ -24,7 +21,7 @@ var scriptsFile = new File($.fileName);
 *                                                                                                                               *
 ******************************************************************************/
 
-function PSD(){
+function PSD(option){
 	this.doc=app.activeDocument;
 	this.docs = app.documents;
 	this.tree = {name:this.doc.name, imgCount:0, childs:[]};
@@ -33,9 +30,19 @@ function PSD(){
 	this.index = -1;
 	this.layers = this.doc.layers;
 	this.option = {
-		exportImages: true		//是否导出图片
+		exportImages: true,		//是否导出图片
+		done: function(){}
 	}
+	if(option){
+		for(k in option){
+			this.option[k] = option[k];
+		}
+	}
+}
 
+PSD.prototype.parseLayers = function(layers, context)  {
+	layers = layers || this.layers;
+	
 	var dir = new Folder((new File($.fileName)).parent.parent+'/output/' + this.getPSDName());
 	dir.create();
 	this.dir = dir;
@@ -43,62 +50,72 @@ function PSD(){
 		this.imagesFolder = new Folder(this.dir + '/images/');
 		this.imagesFolder.create();
 	}
-}
-
-PSD.prototype.parseLayers = function(layers, context)  {
-	layers = layers || this.layers;
 	
 	for(var i = layers.length - 1; i >= 0; i--){
 		this._getLayerInfo(layers[i], context);
 	}
+	this.option.done(this);
 }
 
 PSD.prototype.getWidth = function() {
-	return this.doc.width; 
+	return this.doc.width.value; 
 }
 
 PSD.prototype.getHeight=function() {
-	return this.doc.height ; 
+	return this.doc.height.value; 
 }
 
 PSD.prototype._getLayerInfo=function (layer, context) {
 	this.index++;
 	context = context || this.tree;
 	
-	if(layer.typename == 'ArtLayer'){
+	if(layer.typename === 'ArtLayer' && layer.visible === true){
 			
-		if(layer.visible === true){
+		/* get layer bounds, fix layer bounds */
+		var bounds = layer.bounds,
+			left = bounds[0].value,
+			left = left > 0 ? left : 0;
+			right = bounds[2].value,
+			right = right < this.doc.width.value ? right : this.doc.width.value,
+			top = bounds[1].value,
+			top = top > 0 ? bounds[1].value : 0,
+			bottom = bounds[3].value,
+			bottom = bottom < this.doc.height.value ? bottom : this.doc.height.value;
+
+		if(right > left && bottom > top){
 			var kind = layer.kind.toString();
-			var bounds = layer.bounds;
-			var child = {type:layer.typename, name:layer.name, visible:layer.visible, left:bounds[0].value, top:bounds[1].value, right:bounds[2].value, bottom:bounds[3].value, kind:kind}
+			var child = {type:layer.typename, name:layer.name, visible:layer.visible, left:left, top:top, right:right, bottom:bottom, kind:kind}
 			child.isBackgroundLayer = layer.isBackgroundLayer;
 			child.index = this.index;
 
-		if(kind === 'LayerKind.TEXT'){
-			if(layer.textItem.kind ==TextType.PARAGRAPHTEXT){
-				child.width=layer.textItem.width;
-				child.height=layer.textItem.height;
-			}
-			var textItem = layer.textItem;
-			child.textInfo = {color:textItem.color.rgb.hexValue, contents:textItem.contents, font:textItem.font, size:textItem.size.toString().replace(' ','')};
-			layer.visible = false;
-			this.textLayers.push(layer);
-		}else{
-			if(this.option.exportImages){
-				this.exportImage(layer, this.index);
+			if(kind === 'LayerKind.TEXT'){
+				$.writeln(layer.textItem);
+				if(layer.textItem.kind ==TextType.PARAGRAPHTEXT){
+					child.width=layer.textItem.width;
+					child.height=layer.textItem.height;
+				}
+				var textItem = layer.textItem;
+				child.textInfo = {color:textItem.color.rgb.hexValue, contents:textItem.contents, font:textItem.font, size:textItem.size.toString().replace(' ','')};
+				child.textInfo.bold = textItem.fauxBold;
+				child.textInfo.italic = textItem.fauxItalic;
+				child.textInfo.indent = textItem.firstLineIndent.value + textItem.firstLineIndent.type;
+				child.textInfo.lineHeight = textItem.leading.value + textItem.leading.type;
+				layer.visible = false;
+				this.textLayers.push(layer);
+			}else{
 				this.tree.imgCount++;
+				if(this.option.exportImages){
+					this.exportImage(layer, this.index);
+				}
 			}
 		}
 
 		context.childs.push(child);
-		}
-	}else if(layer.typename == 'LayerSet'){
+	}else if(layer.typename == 'LayerSet' && layer.visible === true){
 			
-		if(layer.visible === true){
-			var o = {type:layer.typename, name:layer.name, index:this.index, childs:[]};
-			context.childs.push(o);
-			this.parseLayers(layer.layers, o);
-		}
+		var o = {type:layer.typename, name:layer.name, index:this.index, childs:[]};
+		context.childs.push(o);
+		this.parseLayers(layer.layers, o);
 	}
         
 }
@@ -131,7 +148,7 @@ PSD.prototype.exportImage = function(layer, index){
 		options.format = SaveDocumentType.PNG;
 		newDoc.exportDocument (img, ExportType.SAVEFORWEB, options);
 		newDoc.close(SaveOptions.DONOTSAVECHANGES);
-	}catch(e){	//目前发现具有蒙层的图层无法执行layer.copy();
+	}catch(e){	//TODO 目前发现具有蒙层的图层无法执行layer.copy();
 		alert(e+'#####'+layer.name);
 	}
 }
