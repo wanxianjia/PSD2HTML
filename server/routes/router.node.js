0102aa34b39e4a6208527d5855f1455bb6ef2465
Justin Ma <hustcer@gmail.com>
2012-05-18 17:27:29 +0800

    format code

--- server/routes/router.node.js
+++ server/routes/router.node.js
@@ -6,18 +6,18 @@
  */
 var gRouter = exports.gRouter       = {};
 
-gRouter["/"]                        = require('./index.node.js').index;				// 首页
-gRouter["/index"]                   = gRouter["/"];									// 首页
+gRouter["/"]                        = require('./index.node.js').index;             // 首页
+gRouter["/index"]                   = gRouter["/"];                                 // 首页
 
-gRouter["/err404"]                  = require('./err404.node.js').err404;			// 404页面
-gRouter["/*"]                       = gRouter["/err404"];							// 其他页面跳转到404
+gRouter["/err404"]                  = require('./err404.node.js').err404;           // 404页面
+gRouter["/*"]                       = gRouter["/err404"];                           // 其他页面跳转到404
 
 /**
  * 提交表单路径映射
  */
 var pRouter = exports.pRouter       = {};
 
-pRouter["/upload"]                  = require('./index.node.js').upload;			// 申请表单提交
+pRouter["/upload"]                  = require('./index.node.js').upload;            // 申请表单提交
 
 
 
