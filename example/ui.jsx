9f6323e44a45549f3bb15b882c86c9aad3609e0c
wanxianjia <okwxj@qq.com>
2012-05-20 12:07:53 +0800

    create ui for setting

---
+++ example/ui.jsx
@@ -0,0 +1,21 @@
+﻿var ui = 'dialog{\
+	text:"设置",\
+	option: Group{\
+		builder:Panel{\
+			orientation:"row",\
+			text:"请选择解析器",\
+			a: Group{\
+				r: RadioButton{text:"EDM"},\
+				r: RadioButton{text:"论坛帖"},\
+				r: RadioButton{text:"静态页"}\
+			}\
+		}\
+	},\
+	buttons:Group{\
+		ok: Button{text:"确定",  properties:{name:"ok"}},\
+		cancel: Button{text:"取消",  properties:{name:"cancel"}}\
+	}\
+}';
+
+var win = new Window(ui);
+win.show();
