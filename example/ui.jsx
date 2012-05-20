ed3889300444690ddb3b2687d969d58803cb9fc2
wanxianjia <okwxj@qq.com>
2012-05-20 12:34:16 +0800

    added evevt on ui

--- example/ui.jsx
+++ example/ui.jsx
@@ -1,14 +1,20 @@
 ﻿var ui = 'dialog{\
 	text:"设置",\
 	option: Group{\
+		orientation:"column",\
 		builder:Panel{\
-			orientation:"row",\
+			size:[284, 50]\
 			text:"请选择解析器",\
 			a: Group{\
 				r: RadioButton{text:"EDM"},\
 				r: RadioButton{text:"论坛帖"},\
 				r: RadioButton{text:"静态页"}\
 			}\
+		},\
+		output:Group{\
+			orientation:"row",\
+			b: Button{text:"选择输出文件夹", properties:{name:"open"}, helpTip:"选择输出文件夹"}\
+			s: EditText  { text:"", preferredSize: [180, 20] },\
 		}\
 	},\
 	buttons:Group{\
@@ -18,4 +24,12 @@
 }';
 
 var win = new Window(ui);
+
+win.option.output.b.onClick = function(){
+	var output = Folder.selectDialog ('选择输出文件夹','~/Documents');
+	if(output){
+		win.option.output.s.text = output;
+	}
+}
+
 win.show();
