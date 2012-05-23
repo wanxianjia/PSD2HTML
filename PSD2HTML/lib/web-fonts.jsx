29c980999a4e2971cdaacc4416c23a91c82a1e50
wanxianjia <okwxj@qq.com>
2012-05-23 16:03:57 +0800

    added font map

--- PSD2HTML/lib/web-fonts.jsx
+++ PSD2HTML/lib/web-fonts.jsx
@@ -1,9 +1,9 @@
 var WEBFONTS = ['Arial', 'Arial Narrow', 'Verdana', 'Georgia', 'Times New Roman', 
 'Trebuchet MS', 'Courier New', 'Impact', 'Comic Sans MS', 'Tahoma', 'Courier', 'Lucida Sans Unicode',
 'Lucida Console', 'Garamond', ' MS Sans Serif', 'MS Serif', 'Palatino Linotype', 'Symbol', '宋体', '微软雅黑', '黑体', '楷体',
-'幼圆', '仿宋', 'MicrosoftYaHei', 'SimSun', 'AdobeSongStd-Light', 'SimHei'];
+'幼圆', '仿宋', 'MicrosoftYaHei', 'SimSun', 'AdobeSongStd-Light', 'SimHei', 'FZLTZHK--GBK1-0'];
 
-WEBFONTS.webFont = {microsoftyahei:'Microsoft YaHei'};
+WEBFONTS.webFont = {microsoftyahei:'Microsoft YaHei', 'fzltzhk--gbk1-0':'Microsoft YaHei'};
 
 WEBFONTS.getWebFont = function(font){
 	return WEBFONTS.webFont[font.toLowerCase()] || font;
