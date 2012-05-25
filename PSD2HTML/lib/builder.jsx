d56df4b79e8db027b89f273e1a1c6e04eb1e167f
xiaowuming <ming@kocm.com>
2012-05-24 23:00:04 -0700

    添加解析到生成HTML的中间件

---
+++ PSD2HTML/lib/builder.jsx
@@ -0,0 +1,28 @@
+﻿// @include "json2-min.jsx"
+// @include "psd.jsx"
+// @include "toHtml.jsx"
+
+//alert(JSON.stringify(APP.OPTION)); //设置面板的设置结果
+var appOp = APP.OPTION;
+
+var psd = new PSD({output:appOp.output});
+psd.parseLayers();
+
+//图片输出设置
+var option = new ExportOptionsSaveForWeb();
+
+if(appOp.image.extension === 'jpg'){
+	option.format = SaveDocumentType.JPEG;
+}else if(appOp.image.extension === 'png'){
+	option.format = SaveDocumentType.PNG;
+	options.PNG8 = appOp.image.png8;
+}
+
+var data = null;
+if(APP.OPTION.builder != "normal"){
+    data = psd.getTextLayersAndSlices(option,psd.getHeight());
+}else{
+   data = psd.getTextLayersAndSlices(option);
+}
+toHtml.init(data,APP,psd);
+//alert(JSON.stringify(data));
