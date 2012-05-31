39b1e5fde9f1fc68b40d8cc2886383847772d35d
wanxianjia <okwxj@qq.com>
2012-05-31 17:09:03 +0800

    import json and image

---
+++ PSD2HTML/builder/importFile.jsx
@@ -0,0 +1,11 @@
+﻿// @include "../lib/psd.jsx"
+
+var psd = new PSD({output:APP.OPTION.output});
+psd.option.exportImages = false;
+psd.parseLayers(null, null, function(layer){
+	if(layer.kind != LayerKind.TEXT && !psd.linkReg.test(layer.name)) return true;
+});
+psd.exportJSON(psd.getTextLayersAndSlices());
+psd.exportPng();
+psd = null;
+alert('done')
