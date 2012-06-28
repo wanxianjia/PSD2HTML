6f32aa1d5254b425de1f220877dbb2eff5f00f38
wanxianjia <okwxj@qq.com>
2012-06-28 10:15:23 +0800

    test

---
+++ test/test-exportSelection.jsx
@@ -0,0 +1,7 @@
+﻿// @include "../PSD2HTML/lib/psd.jsx"
+
+(function(){
+	var psd = new PSD({output:'~/Documents'});
+	var img = psd.exportSelection([[0,0], [100, 0], [100,100], [0,100]]);
+	$.writeln(img);
+})();
