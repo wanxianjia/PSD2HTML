5d44acaf2ee7c08499378d0d65a1a0576118882c
wanxianjia <okwxj@qq.com>
2012-07-18 12:58:47 +0800

    test

---
+++ test/test-selectionIsMonochrome.jsx
@@ -0,0 +1,8 @@
+﻿// @include "../PSD2HTML/lib/psd.jsx"
+
+(function(){
+	var psd = new PSD();
+	//$.writeln(psd.selectionIsMonochrome([[0,0], [psd.getWidth(), 0], [psd.getWidth(), psd.getHeight()], [0, psd.getHeight()]]));
+	$.writeln(psd.selectionIsMonochrome([[0,0], [100, 0], [100, 100], [0, 100]]));
+})();
+
